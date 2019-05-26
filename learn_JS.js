75b67119338accc09afad30480c4218e17619e7b
gblageov <gblagoev.2create@gmail.com>
2019-05-26 12:54:54 +0300

    add JS

---
+++ learn_JS.js
@@ -0,0 +1,10 @@
+function multyTwo(a,b) {
+    return a * b;
+}
+console.log(multyTwo(2,3));
+
+function sayHi(name, age) {
+    return "Hi. My name is " + name + " and I'm " + age + " years old"
+}
+console.log(sayHi("george", 33));
+
